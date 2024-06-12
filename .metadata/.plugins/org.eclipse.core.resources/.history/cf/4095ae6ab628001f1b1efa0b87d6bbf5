d5e16fe91513d3b22c30b4544523617cf652f946
Maliya11 <114799896+Maliya11@users.noreply.github.com>
2024-06-12 18:00:51 +0530

    uart implementation

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/cf/4095ae6ab628001f1b1efa0b87d6bbf5
@@ -0,0 +1,18 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jun 12, 2024
+ *      Author: ACER
+ */
+
+#ifndef UART_H_
+#define UART_H
+
+#include <stdint.h>
+#include "stm32f1xx.h"
+
+void uart1_tx_init(void);
+
+
+
+#endif /* UART_H_ */
---
+++ 7_uart_modular/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/core_cm3.h \
+ C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ ../Inc/uart.h
+C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/core_cm3.h:
+C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+../Inc/uart.h:
