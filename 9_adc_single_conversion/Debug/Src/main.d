15d1a698b9c0d5f29ba03d97b5329312ff6f6f95
Maliya11 <114799896+Maliya11@users.noreply.github.com>
2024-06-14 10:30:14 +0530

    systick in cortex-m3

--- 9_adc_single_conversion/Debug/Src/main.d
+++ 9_adc_single_conversion/Debug/Src/main.d
@@ -6,7 +6,7 @@ Src/main.o: ../Src/main.c \
  C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/cmsis_compiler.h \
  C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/cmsis_gcc.h \
  C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
- ../Inc/adc.h
+ ../Inc/adc.h ../Inc/uart.h
 C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
 C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
 C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/core_cm3.h:
@@ -15,3 +15,4 @@ C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/cmsis_
 C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Include/cmsis_gcc.h:
 C:/Users/ACER/Documents/GitHub/STM32-BareMetal/chip_headers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
 ../Inc/adc.h:
+../Inc/uart.h:
--- 9_adc_single_conversion/Src/main.c
+++ 9_adc_single_conversion/Src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "adc.h"
+#include "uart.h"
 
 
 uint32_t sensor_value;
@@ -10,10 +11,12 @@ int main(void)
 {
 
 	pa1_adc_init();
+	uart1_rxtx_init();
+	start_conversion();
 
 	while(1)
 	{
-		start_conversion();
 		sensor_value = adc_read();
+		printf("Sensor value : %d \n\r", (int)sensor_value);
 	}
 }
