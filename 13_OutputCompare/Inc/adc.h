a7dca4e32245a3c4f1f93a543b8f10dce84c4619
Maliya11 <114799896+Maliya11@users.noreply.github.com>
2024-06-16 09:07:25 +0530

    general purpose timers

---
+++ 13_OutputCompare/Inc/adc.h
@@ -0,0 +1,16 @@
+/*
+ * adc.h
+ *
+ *  Created on: Jun 11, 2024
+ *      Author: ACER
+ */
+
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <stdint.h>
+void pa1_adc_init(void);
+void start_conversion(void);
+uint32_t adc_read();
+
+#endif /* ADC_H_ */
